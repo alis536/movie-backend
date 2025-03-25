e08ac4a4f944db2136e9ef4b1b2b950ecb4572d3
Alisher555-Noob <alirizojonkarimov@gamil.com>
2025-03-25 18:25:56 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://172.20.10.3:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+let onlineUsers = {};
+let roomUsers = new Set();
+let chatHistory = [];
+
+io.on("connection", (socket) => {
+  console.log("Пользователь подключился", socket.id);
+
+  socket.on("join", (name) => {
+    onlineUsers[socket.id] = name;
+    io.emit("updateUsers", Object.values(onlineUsers));
+  });
+
+  socket.on("enterRoom", (name) => {
+    roomUsers.add(name);
+    io.emit("updateRoomUsers", Array.from(roomUsers));
+    socket.emit("chatHistory", chatHistory);
+  });
+
+  socket.on("leaveRoom", (name) => {
+    roomUsers.delete(name);
+    io.emit("updateRoomUsers", Array.from(roomUsers));
+  });
+
+  socket.on("sendMessage", (message) => {
+    chatHistory.push(message);
+    io.emit("receiveMessage", message);
+  });
+
+  // Запуск фильма
+  socket.on("startMovie", () => {
+    io.emit("playMovie");
+  });
+
+  // Остановка фильма
+  socket.on("pauseMovie", () => {
+    io.emit("pauseMovie");
+  });
+
+  socket.on("disconnect", () => {
+    if (onlineUsers[socket.id]) {
+      roomUsers.delete(onlineUsers[socket.id]);
+      delete onlineUsers[socket.id];
+      io.emit("updateUsers", Object.values(onlineUsers));
+      io.emit("updateRoomUsers", Array.from(roomUsers));
+    }
+    console.log("Пользователь отключился", socket.id);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("Сервер запущен на порту 4000");
+});
